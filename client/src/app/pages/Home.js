2392d19d68a4b369bef04f3f00ecc6195bed45d2
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-05-05 11:04:13 -0700

    Initial changes to make it skyward

--- client/src/app/pages/Home.js
+++ client/src/app/pages/Home.js
@@ -6,8 +6,18 @@ class Home extends Component {
     return (
       <div id="homeContainer">
 
-        <h1 className="homeBox homeHeader">Hello World!</h1>
-        <p className="homeBox homeText">This is the Home page.</p>
+        <div id="homeBoxOne" className="homeBox">
+          <img id="homeImage" src="/images/skyward_tree_003.jpg" alt="skywardTree"/>
+          <h1 id="homeImageText">Skyward Therapies</h1>
+        </div>
+
+        <div className="homeBoxTwo" className="homeBox">
+          <p></p>
+        </div>
+
+        <div className="homeBoxThree" className="homeBox"></div>
+
+        <div className="homeBoxfour" className="homeBox"></div>
 
       </div>
     );
