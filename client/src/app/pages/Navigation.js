6d195100c931895a2f97c532fa1528f85a01c4c5
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-05-10 11:16:01 -0700

    Nav Stylign done

--- client/src/app/pages/Navigation.js
+++ client/src/app/pages/Navigation.js
@@ -6,67 +6,50 @@ class Navigation extends Component {
   constructor(props){
     super(props);
     this.state = {
-      showMenu: true
     };
   }
 
-  toggleShowMenu = async () => {
-    // Set the state of 'showMenu' to false IF it is currently TRUE.
-    if (this.state.showMenu) {
-      this.setState({ showMenu: false })
-    } else {
-      // Set the state of 'showMenu' to true IF it is currently FALSE.
-      this.setState({ showMenu: true })
-    }
-  }
 
   render() {
 
     return (
     <div id="naviContainer">
 
-      <div className={`naviLeafContainer ${this.state.showMenu ? "slideOutToRight" : "slideInFromRight"}`}>
-        <div className="leaf">
-          <div class="hamburger" onClick={this.toggleShowMenu}></div>
+      <Link to={'./'} className="naviLink naviLinkHighlightBlue">
+        <div className="naviLinkText borderBlue">
+          Home
         </div>
-      </div>
-
-      <div className={`naviDisplayContainer ${this.state.showMenu ? "slideInFromTop" : "slideOutToTop"}`} >
-
-        <Link to={'./'} className="naviLink2">
-          <div className="naviLinkText2 borderBlue">
-            Home
-          </div>
-        </Link>
-        <Link to={'./'} className="naviLink2">
-          <div className="naviLinkText2 borderBlue">
-            Home
-          </div>
-        </Link>
-        <Link to={'./'} className="naviLink2">
-          <div className="naviLinkText2 borderBlue">
-            Home
-          </div>
-        </Link>
-        <Link to={'./'} className="naviLink2">
-          <div className="naviLinkText2 borderBlue">
-            Home
-          </div>
-        </Link>
-        <Link to={'./'} className="naviLink2">
-          <div className="naviLinkText2 borderBlue">
-            Home
-          </div>
-        </Link>
-
-
-
-          <div className="naviLink naviLinkHighlightWhite" onClick={this.toggleShowMenu}>
-            <div className="naviArrow borderWhite"></div>
-          </div>
-
-
-      </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightGreen">
+        <div className="naviLinkText borderGreen">
+          Home
+        </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightYellow">
+        <div className="naviLinkText borderYellow">
+          Home
+        </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightRed">
+        <div className="naviLinkText borderRed">
+          Home
+        </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightPurple">
+        <div className="naviLinkText borderPurple">
+          Home
+        </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightBrown">
+        <div className="naviLinkText borderBrown">
+          Home
+        </div>
+      </Link>
+      <Link to={'./'} className="naviLink naviLinkHighlightWhite">
+        <div className="naviLinkText borderWhite">
+          Home
+        </div>
+      </Link>
 
     </div>
     );
