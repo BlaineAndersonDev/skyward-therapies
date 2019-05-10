599e98055ee43f130ea121600f0a1ffb0d6e10ed
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-05-10 14:11:15 -0700

    Setup most content

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Construction from './pages/construction/Construction.js';
 import Home from './pages/home/Home.js';
-import About from './pages/about/About.js';
-// import Footer from './pages/Footer.js';
 import './App.css';
 
 class App extends Component {
@@ -15,10 +13,6 @@ class App extends Component {
           <div id="appBody">
             <Route exact path='/' component={Construction}/>
             <Route path='/home' component={Home}/>
-            <Route
-              path="/about"
-              render={(props) => <About {...props} />}
-            />
           </div>
 
         </div>
