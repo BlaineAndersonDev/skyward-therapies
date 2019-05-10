5fcdc489724cb61e0c99225c6f4d7c1a5210d8c8
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-05-10 10:23:20 -0700

    Updated Contruction to be iffnalized for now

--- client/src/app/pages/construction/Construction.js
+++ client/src/app/pages/construction/Construction.js
@@ -7,9 +7,20 @@ class Construction extends Component {
     return (
     <div id="conContainer">
       <div id="conTextBox">
-        <h1 className="conText">
+
+        <div className="conBox">
           Skyward Therapies is currently under construction
-        </h1>
+        </div>
+
+        <div className="conBox">
+          Please visit our Facebook or Instagram page!
+        </div>
+
+        <div className="conBox">
+          <a href="https://www.facebook.com/skywardtherapies" target="_blank" className="conLink">Facebook</a>
+          <a href="https://www.instagram.com/skywardtherapiesllc" target="_blank" className="conLink">Instagram</a>
+        </div>
+
       </div>
     </div>
     );
