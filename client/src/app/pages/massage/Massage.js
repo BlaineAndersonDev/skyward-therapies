5cf6493008d32b7b80d12bedb027379edb2ab2ec
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-06-08 16:06:12 -0700

    Updated Massage stylings

--- client/src/app/pages/massage/Massage.js
+++ client/src/app/pages/massage/Massage.js
@@ -7,29 +7,29 @@ class Massage extends Component {
     <div id="massageContainer" className="comContainer">
       <h1 id="massageSectiontitle" className="comSectiontitle">Massage Styles</h1>
 
-      <div className="comSectionContainer">
-        <div className="comSubSectionContainer">
-          <h3 className="comSubSectionTitle">Swedish Relaxation</h3>
+      <div className="comSubSectionBoxContainer">
+        <div className="comSubSectionBox massageSubSectionBox">
+          <h3 className="comSubSectionBoxTitle massageSubSectionBoxTitle">Swedish Relaxation</h3>
           <p className="comSubSectionText">When most people think of massage, swedish massage is often times what comes to mind first. Made famous in spas, swedish is relaxing, quiet, and soothing. Usually performed over the course of an hour it utilizes long even strokes to calm the mind and gently release surface tension in muscles.</p>
         </div>
 
-        <div className="comSubSectionContainer">
-          <h3 className="comSubSectionTitle">Hot Stone Massage</h3>
+        <div className="comSubSectionBox massageSubSectionBox">
+          <h3 className="comSubSectionBoxTitle massageSubSectionBoxTitle">Hot Stone Massage</h3>
           <p className="comSubSectionText">Gently heated stones are used in this massage style to increase blood circulation and calmly release tension stricken muscle fibers.</p>
         </div>
 
-        <div className="comSubSectionContainer">
-          <h3 className="comSubSectionTitle">Deep Tissue</h3>
+        <div className="comSubSectionBox massageSubSectionBox">
+          <h3 className="comSubSectionBoxTitle massageSubSectionBoxTitle">Deep Tissue</h3>
           <p className="comSubSectionText">Infamously known as the “work the knots out” massage type. Many people ask for this modality, specifically seeking the release of the deeper layers of muscle that can hold the most tension. This can be an uncomfortable therapy session and typically is performed on targeted areas of the body that “pull” on one another.</p>
         </div>
 
-        <div className="comSubSectionContainer">
-          <h3 className="comSubSectionTitle">Prenatal</h3>
+        <div className="comSubSectionBox massageSubSectionBox">
+          <h3 className="comSubSectionBoxTitle massageSubSectionBoxTitle">Prenatal</h3>
           <p className="comSubSectionText">Moms-to-be need special care and specific techniques. Primarily this type of massage is performed in the 2nd and 3rd trimesters when ligaments are stretching and more discomfort is overcoming the body.</p>
         </div>
 
-        <div className="comSubSectionContainer">
-          <h3 className="comSubSectionTitle">Trigger Point</h3>
+        <div className="comSubSectionBox massageSubSectionBox">
+          <h3 className="comSubSectionBoxTitle massageSubSectionBoxTitle">Trigger Point</h3>
           <p className="comSubSectionText">A targeted therapy, similar in discomfort to deep tissue, trigger point therapy is best utilized for someone who is experiencing referring pain that is coming from another part of the body. Example: A pinched sciatic nerve causing shooting pains down the legs.</p>
         </div>
 
--- client/src/app/pages/mission/Mission.js
+++ client/src/app/pages/mission/Mission.js
@@ -5,12 +5,15 @@ class Mission extends Component {
   render() {
     return (
     <div id="missionContainer" className="comContainer">
-      <h1 id="missionSectiontitle" className="comSectiontitle">Our Mission</h1>
+      <h1 id="missionSectiontitle" className="comSectiontitle">
+        ~We Support <br /> <span className="comTextUnderline">You</span>~
+      </h1>
 
-      <div className="comSectionContainer">
+      <div className="comSectionContainer comRows">
         <p className="comSubSectionText">
           Skyward Therapies LLC is the culmination of over a decade of hard work focusing on helping individual people with the best possible personalized care.
         </p>
+        <span className="comVerticalDivider" />
         <p className="comSubSectionText">
           Our tagline: "Back to Balance" isn't just about losing pounds or permanently curing your tension headaches, it’s about making you a priority and having a supportive network behind you that is there to help you make the healthy-lifestyle changes that will allow you achieve the balance you have been looking for.
         </p>
