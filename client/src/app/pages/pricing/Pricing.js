9d40661a28e825ac0836cfe66d280b6e40f549be
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-06-08 18:29:14 -0700

    Reworked Pricing section

--- client/src/app/pages/pricing/Pricing.js
+++ client/src/app/pages/pricing/Pricing.js
@@ -6,15 +6,87 @@ class Pricing extends Component {
     return (
     <div id="pricingContainer" className="comContainer">
       <h1 id="pricingSectiontitle" className="comSectiontitle">Pricing</h1>
-      <div className="comSubSectionContainer">
-        <p className="comSubSectionText">No matter the type of massage, sessions at Skyward Therapy are always one price and always the full time:</p>
-        <ul>
-          <li className="comSubSectionText">30 Minutes: $35</li>
-          <li className="comSubSectionText">60 Minutes: $65</li>
-          <li className="comSubSectionText">90 Minutes: $100</li>
-          <li className="comSubSectionText">120 Minutes: $130</li>
-        </ul>
+      <div className="pricingBoxContainer">
+
+        <div className="pricingBox">
+          <div className="pricingIconContainer">
+            <img src="images/skyward_tree_cycle_01.png" alt="tree_cycle_01" className="pricingIcon" />
+          </div>
+          <div id="pricingBoxColor1" className="pricingBoxTitleContainer">
+            <p className="pricingBoxTitlePrice">$35</p>
+            <h1 className="pricingBoxTitleNumber">30</h1>
+            <h1 className="pricingBoxTitleText">Minutes</h1>
+          </div>
+          <ul className="pricingIncludedContainer">
+            <li className="pricingIncludedTitle">Includes:</li>
+            <li className="pricingIncludedItem pricingTextColor1">Customizable Care</li>
+            <li className="pricingIncludedItem pricingTextColor1">Hot Towels</li>
+            <li className="pricingIncludedItem pricingTextColor1">Aromatherapy</li>
+            <li className="pricingIncludedItem pricingTextColor1">Hot Stones</li>
+          </ul>
+        </div>
+
+        <div className="pricingBox">
+          <div className="pricingIconContainer">
+            <img src="images/skyward_tree_cycle_02.png" alt="tree_cycle_02" className="pricingIcon" />
+          </div>
+          <div id="pricingBoxColor2" className="pricingBoxTitleContainer">
+            <p className="pricingBoxTitlePrice">$65</p>
+            <h1 className="pricingBoxTitleNumber">60</h1>
+            <h1 className="pricingBoxTitleText">Minutes</h1>
+          </div>
+          <ul className="pricingIncludedContainer">
+            <li className="pricingIncludedTitle">Includes:</li>
+            <li className="pricingIncludedItem pricingTextColor2">Customizable Care</li>
+            <li className="pricingIncludedItem pricingTextColor2">Hot Towels</li>
+            <li className="pricingIncludedItem pricingTextColor2">Aromatherapy</li>
+            <li className="pricingIncludedItem pricingTextColor2">Hot Stones</li>
+          </ul>
+        </div>
+
+        <div className="pricingBox">
+          <div className="pricingIconContainer">
+            <img src="images/skyward_tree_cycle_03.png" alt="tree_cycle_03" className="pricingIcon" />
+          </div>
+          <div id="pricingBoxColor3" className="pricingBoxTitleContainer">
+            <p className="pricingBoxTitlePrice">$100</p>
+            <h1 className="pricingBoxTitleNumber">90</h1>
+            <h1 className="pricingBoxTitleText">Minutes</h1>
+          </div>
+          <ul className="pricingIncludedContainer">
+            <li className="pricingIncludedTitle">Includes:</li>
+            <li className="pricingIncludedItem pricingTextColor3">Customizable Care</li>
+            <li className="pricingIncludedItem pricingTextColor3">Hot Towels</li>
+            <li className="pricingIncludedItem pricingTextColor3">Aromatherapy</li>
+            <li className="pricingIncludedItem pricingTextColor3">Hot Stones</li>
+          </ul>
+        </div>
+
+        <div className="pricingBox">
+          <div className="pricingIconContainer">
+            <img src="images/skyward_tree_cycle_04.png" alt="tree_cycle_04" className="pricingIcon" />
+          </div>
+          <div id="pricingBoxColor4" className="pricingBoxTitleContainer">
+            <p className="pricingBoxTitlePrice">$130</p>
+            <h1 className="pricingBoxTitleNumber">120</h1>
+            <h1 className="pricingBoxTitleText">Minutes</h1>
+          </div>
+          <ul className="pricingIncludedContainer">
+            <li className="pricingIncludedTitle">Includes:</li>
+            <li className="pricingIncludedItem pricingTextColor4">Customizable Care</li>
+            <li className="pricingIncludedItem pricingTextColor4">Hot Towels</li>
+            <li className="pricingIncludedItem pricingTextColor4">Aromatherapy</li>
+            <li className="pricingIncludedItem pricingTextColor4">Hot Stones</li>
+          </ul>
+        </div>
       </div>
+
+      <div className="comSectionContainer">
+        <div className="comSubSectionContainer">
+          <p className="comSubSectionText">Skyward Therapy proudly provides all listed services to our clients for any length of massage!</p>
+        </div>
+      </div>
+
     </div>
     );
   }
