90eb15b0bc0e0c25ba42a6f49c3a98b9f729836e
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-06-12 16:53:50 -0700

    Begun logic for message system in footer

--- client/src/app/footer/links/Links.js
+++ client/src/app/footer/links/Links.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { HashLink as NavLink } from 'react-router-hash-link';
 import './Links.css';
 
 class Links extends Component {
+
   render() {
     return (
     <div id="footLinksContainer">
-      Links Container
+
+      <div className="footLinksSide">
+        <div className="footLinksSection">
+          <h3 className="footLinksTitle">Information:</h3>
+          <NavLink smooth to="/#landing" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Home </div>
+          </NavLink>
+          <NavLink smooth to="/#massage" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Our Mission </div>
+          </NavLink>
+          <NavLink smooth to="/#massage" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Massage Styles </div>
+          </NavLink>
+          <NavLink smooth to="/#pricing" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Special Modalities </div>
+          </NavLink>
+          <NavLink smooth to="/#contact" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Pricing </div>
+          </NavLink>
+          <NavLink smooth to="/#contact" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> Schedual a Massage </div>
+          </NavLink>
+          <NavLink smooth to="/#about" className="footLinksNavLink">
+            <div className="footLinksNavLinkText"> About </div>
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="footLinksSide">
+        <div className="footLinksSection">
+          <h3 className="footLinksTitle">Address:</h3>
+          <p className="footLinksText">6239 E Brown Rd Suite 112,</p>
+          <p className="footLinksText">Mesa, AZ 85205</p>
+        </div>
+      </div>
+
     </div>
     );
   }
--- client/src/app/footer/messages/Messages.js
+++ client/src/app/footer/messages/Messages.js
@@ -1,14 +1,243 @@
 import React, { Component } from 'react';
 import './Messages.css';
+import axios from 'axios';
 
 class Messages extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      nameError: '',
+      emailError: '',
+      messageError: '',
+      nameValid: false,
+      emailValid: false,
+      messageValid: false,
+    };
+  };
+
+  handleNameChange = (event) => {
+    this.setState({name: event.target.value})
+  };
+  handleEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  };
+  handleMessageChange = (event) => {
+    this.setState({message: event.target.value})
+  };
+
+  // Standard Error Check Functions.
+  errorCheckStringNullEmpty = (string) => {
+    if (string === null || string === '') {
+      return true
+    } else {
+      return false
+    }
+  };
+  errorCheckStringNormalCharacters = (string) => {
+    if (!string.match(/^[A-Za-z0-9]+(?:[_][A-Za-z0-9]+)*$/)) {
+      return true
+    } else {
+      return false
+    }
+  };
+  errorCheckEmail = (email) => {
+    if (!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)) {
+      return true
+    } else {
+      return false
+    }
+  };
+
+  // State Check Functions.
+  checkName = () => {
+    let newName = this.state.name;
+    // Error if Name is empty.
+    if (this.errorCheckStringNullEmpty(newName)) {
+      this.setState({
+        nameError: 'Name cannot be empty.',
+        nameValid: false
+      })
+    }
+    // Error if Name has illegal characters.
+    else if (this.errorCheckStringNormalCharacters(newName)) {
+      this.setState({
+        nameError: 'Name may only contain letters, numbers & underscores.',
+        nameValid: false
+      })
+    }
+    // Validate that Name is good to use.
+    else {
+      this.setState({
+        nameError: null,
+        nameValid: true
+      });
+    };
+  };
+
+  checkEmail = () => {
+    let newEmail = this.state.email;
+    // Error if Email is empty.
+    if (this.errorCheckStringNullEmpty(newEmail)) {
+      this.setState({
+        emailError: 'Email cannot be empty.',
+        emailValid: false
+      })
+    }
+    // Error if Email has illegal characters.
+    else if (this.errorCheckEmail(newEmail)) {
+      this.setState({
+        emailError: "Improper Email.",
+        emailValid: false
+      })
+    }
+    // Validate that Email is good to use.
+    else {
+      this.setState({
+        emailError: null,
+        emailValid: true
+      });
+    };
+  };
+
+  // State Check Functions.
+  checkMessage = () => {
+    let newMessage = this.state.message;
+    // Error if Message is empty.
+    if (this.errorCheckStringNullEmpty(newMessage)) {
+      this.setState({
+        messageError: 'Message cannot be empty.',
+        messageValid: false
+      })
+    }
+    // Error if Message has illegal characters.
+    else if (this.errorCheckStringNormalCharacters(newMessage)) {
+      this.setState({
+        messageError: 'Message may only contain letters, numbers & underscores.',
+        messageValid: false
+      })
+    }
+    // Validate that Message is good to use.
+    else {
+      this.setState({
+        messageError: null,
+        messageValid: true
+      });
+    };
+  };
+
+  handleSubmit = async (event) => {
+    console.log('Enter handleSubmit')
+    event.preventDefault()
+    await this.checkName();
+    await this.checkEmail();
+    await this.checkMessage();
+    await this.checkIfValid();
+  };
+
+  checkIfValid = () => {
+    if (this.state.nameValid &&
+        this.state.emailValid &&
+        this.state.messageValid
+      ) {
+      this.sendQuestion();
+    }
+  }
+
+  sendQuestion = () => {
+    console.log('Enter sendQuestion')
+    const questionToSend = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message
+    }
+    // SEND MESSAGE AT THIS POINT
+  }
+
   render() {
+    let nameTitleDisplay;
+    if (this.state.nameError === '') { // If nameError is empty
+      nameTitleDisplay = (
+        <label className="footMessageInputItem"> Name: </label>
+      )
+    } else { // If nameError is NOT empty
+      nameTitleDisplay = (
+        <label className="footMessageInputItem">
+          <span className="footMessageInputItem" style={{color: "red"}}>
+            {this.state.nameError}
+          </span>
+        </label>
+      )
+    }
+
+    let emailTitleDisplay;
+    if (this.state.emailError === '') { // If emailError is empty
+      emailTitleDisplay = (
+        <label className="footMessageInputItem"> Email: </label>
+      )
+    } else { // If emailError is NOT empty
+      emailTitleDisplay = (
+        <label className="footMessageInputItem">
+          <span className="footMessageInputItem" style={{color: "red"}}>
+            {this.state.emailError}
+          </span>
+        </label>
+      )
+    }
+
+    let messageTitleDisplay;
+    if (this.state.messageError === '') { // If messageError is empty
+      messageTitleDisplay = (
+        <label className="footMessageInputItem"> Message: </label>
+      )
+    } else { // If messageError is NOT empty
+      messageTitleDisplay = (
+        <label className="footMessageInputItem">
+          <span className="footMessageInputItem" style={{color: "red"}}>
+            {this.state.messageError}
+          </span>
+        </label>
+      )
+    }
+
+
     return (
     <div id="footMessageContainer">
-      Messages Container
+      <form id="footMessageFormContainer" onSubmit={this.handleSubmit}>
+
+        <div id="footMessageNameContainer" className="footMessageInputContainer">
+          {nameTitleDisplay}
+          <input
+            className="footMessageInputItem footMessageInputBox"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleNameChange} />
+        </div>
+
+        <div id="footMessageEmailContainer" className="footMessageInputContainer">
+          {emailTitleDisplay}
+          <input
+            type="text"
+            value={this.state.email}
+            onChange={this.handleEmailChange} />
+        </div>
+
+        <div id="footMessageTextContainer" className="footMessageInputContainer">
+          {messageTitleDisplay}
+          <textarea
+            type="text"
+            value={this.state.message}
+            onChange={this.handleMessageChange} />
+        </div>
+
+        <input type="submit" value="Submit" />
+
+      </form>
     </div>
     );
   }
-}
+};
 
 export default Messages;
