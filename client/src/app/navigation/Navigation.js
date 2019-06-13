4a9d05d589962be44886ce0ebefa39ff466b2a73
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-06-13 03:26:59 -0700

    Styles complete for mid size media

--- client/src/app/navigation/Navigation.js
+++ client/src/app/navigation/Navigation.js
@@ -1,42 +1,24 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { HashLink as NavLink } from 'react-router-hash-link';
 import './Navigation.css';
 
 class Navigation extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    };
-  }
-
-  holdingFunction = () => {
-    return (
-      <Link to={'./'} className="naviLink naviLinkHighlightBlue">
-        <div className="naviLinkText">
-          Photos
-        </div>
-      </Link>
-    )
-  }
-
   render() {
-
     return (
     <div id="naviContainer">
-      <NavLink smooth to="/#landing" className="naviLink naviLinkHighlightBlue">
+      <NavLink smooth to="/#landing" className="naviLinkWrapper">
         <div className="naviLinkText"> Home </div>
       </NavLink>
-      <NavLink smooth to="/#massage" className="naviLink naviLinkHighlightBlue">
+      <NavLink smooth to="/#massage" className="naviLinkWrapper">
         <div className="naviLinkText"> Massage </div>
       </NavLink>
-      <NavLink smooth to="/#pricing" className="naviLink naviLinkHighlightBlue">
+      <NavLink smooth to="/#pricing" className="naviLinkWrapper">
         <div className="naviLinkText"> Pricing </div>
       </NavLink>
-      <NavLink smooth to="/#contact" className="naviLink naviLinkHighlightBlue">
+      <NavLink smooth to="/#contact" className="naviLinkWrapper">
         <div className="naviLinkText"> Contact </div>
       </NavLink>
-      <NavLink smooth to="/#about" className="naviLink naviLinkHighlightBlue">
+      <NavLink smooth to="/#about" className="naviLinkWrapper">
         <div className="naviLinkText"> About </div>
       </NavLink>
     </div>
