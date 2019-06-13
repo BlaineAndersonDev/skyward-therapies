dc8c524f41da8379016c6a328b91772f250bdad7
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-06-13 02:39:12 -0700

    Footer overhaul mostly complete

--- client/src/app/footer/links/social/Social.js
+++ client/src/app/footer/links/social/Social.js
@@ -2,11 +2,50 @@ import React, { Component } from 'react';
 import './Social.css';
 
 class Social extends Component {
-
   render() {
     return (
     <div id="footSocialContainer">
-      Social Container
+
+      <a href="https://www.facebook.com/SkywardTherapies/" target="_blank" rel="noopener noreferrer" className="socialIconContainer">
+        <img
+          src="images/social_icons/sky_social_icon_white_facebook.png"
+          alt="Skyward Facebook"
+          className="socialIcon socialIconFacebook"
+        />
+      </a>
+
+      <a href="https://www.instagram.com/skywardtherapiesllc/" target="_blank" rel="noopener noreferrer" className="socialIconContainer">
+        <img
+          src="images/social_icons/sky_social_icon_white_instagram.png"
+          alt="Skyward Instagram"
+          className="socialIcon socialIconInstagram"
+        />
+      </a>
+
+      <a href="" target="_blank" rel="noopener noreferrer" className="socialIconContainer">
+        <img
+          src="images/social_icons/sky_social_icon_white_snapchat.png"
+          alt="Skyward Snapchat"
+          className="socialIcon socialIconSnapchat"
+        />
+      </a>
+
+      <a href="" target="_blank" rel="noopener noreferrer" className="socialIconContainer">
+        <img
+          src="images/social_icons/sky_social_icon_white_twitter.png"
+          alt="Skyward Twitter"
+          className="socialIcon socialIconTwitter"
+        />
+      </a>
+
+      <a href="" target="_blank" rel="noopener noreferrer" className="socialIconContainer">
+        <img
+          src="images/social_icons/sky_social_icon_white_linkedin.png"
+          alt="Skyward Linkedin"
+          className="socialIcon socialIconLinkedin"
+        />
+      </a>
+
     </div>
     );
   }
