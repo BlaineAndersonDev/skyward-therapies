c351b7a33d586d78e5252e4d45e0ef29ce1eca76
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-07-22 15:43:28 -0700

    Updated Mission to have a single box instead of 2

--- client/src/app/pages/mission/Mission.js
+++ client/src/app/pages/mission/Mission.js
@@ -16,9 +16,6 @@ class Mission extends Component {
           <p className="missionText">
             Skyward Therapies LLC is the culmination of over a decade of hard work focusing on helping individual people with the best possible personalized care.
           </p>
-        </div>
-
-        <div className="missionBackground1">
           <p className="missionText">
             Our tagline: "Back to Balance" isn't just about losing pounds or permanently curing your tension headaches, it’s about making you a priority and having a supportive network behind you that is there to help you make the healthy-lifestyle changes that will allow you achieve the balance you have been looking for.
           </p>
