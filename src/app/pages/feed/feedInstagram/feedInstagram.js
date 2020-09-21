52b88adcfd6281aef88976c4296caeb366067a37
Blaine Anderson <BlaineAndersonDev@Gmail.com>
2020-09-21 14:52:34 -0700

    Begun work on instagram carousel

--- src/app/pages/feed/feedInstagram/feedInstagram.js
+++ src/app/pages/feed/feedInstagram/feedInstagram.js
@@ -4,13 +4,13 @@ import Slide from 'react-reveal/Slide';
 import "./feedInstagram.css";
 
 const CarouselUI = ({ position, total, handleClick, children }) => (
-  <div className="fGRCarouselContainer">
-    <div className="fGRCarouselChildren">
+  <div className="fICarouselContainer">
+    <div className="fICarouselChildren">
       {children}
     </div>
-    <div className="fGRCarouselDots">
+    <div className="fICarouselDots">
       {Array(...Array(total)).map((val, index) =>
-        <div className="fGRCarouselDot" key={index} onClick={handleClick} data-position={index}>
+        <div className="fICarouselDot" key={index} onClick={handleClick} data-position={index}>
           {index === position ? '● ' : '○ '}
         </div>
       )}
@@ -23,12 +23,16 @@ class feedInstagram extends React.Component {
   render() {
     return (
       <div className="feedInstagram">
-        <Carousel defaultWait={10000} maxTurns={10} swipe={true}>
+        <Carousel defaultWait={5000} maxTurns={10} swipe={true}>
             <Slide right>
-              <div className="fGRItemContainer">
-                <p className="fGRItemText">Ex1</p>
-                <p className="fGRItemName">Ex2</p>
-              </div>
+              <a href="https://www.instagram.com/p/B5osJSAlOuu/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_001.jpg" className="fIItem" alt="Instagram_001" />
+              </a>
+            </Slide>
+            <Slide right>
+              <a href="_________" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_002.jpg" className="fIItem" alt="Instagram_001" />
+              </a>
             </Slide>
         </Carousel>
       </div>
