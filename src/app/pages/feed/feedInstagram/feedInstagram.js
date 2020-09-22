c4f27441409977c6398e1740b0c0e94d45788aa1
Blaine Anderson <BlaineAndersonDev@Gmail.com>
2020-09-22 11:26:30 -0700

    Finalized instagram carousel

--- src/app/pages/feed/feedInstagram/feedInstagram.js
+++ src/app/pages/feed/feedInstagram/feedInstagram.js
@@ -23,15 +23,31 @@ class feedInstagram extends React.Component {
   render() {
     return (
       <div className="feedInstagram">
-        <Carousel defaultWait={5000} maxTurns={10} swipe={true}>
+        <h1 className="fITitle">Follow us on Instagram: <a href="https://www.instagram.com/skyward.therapies.nutrition/" target="_blank" rel="noopener noreferrer" className="fITitleButton">@skyward.therapies.nutrition</a></h1>
+        <Carousel defaultWait={10000} maxTurns={10} swipe={true}>
             <Slide right>
-              <a href="https://www.instagram.com/p/B5osJSAlOuu/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.instagram.com/p/CECye1gJfVO/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
                 <img src="/images/pages/insta_001.jpg" className="fIItem" alt="Instagram_001" />
               </a>
             </Slide>
             <Slide right>
-              <a href="_________" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
-                <img src="/images/pages/insta_002.jpg" className="fIItem" alt="Instagram_001" />
+              <a href="https://www.instagram.com/p/B5osJSAlOuu/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_002.jpg" className="fIItem" alt="Instagram_002" />
+              </a>
+            </Slide>
+            <Slide right>
+              <a href="https://www.instagram.com/p/B_bag3hp1lG/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_003.jpg" className="fIItem" alt="Instagram_003" />
+              </a>
+            </Slide>
+            <Slide right>
+              <a href="https://www.instagram.com/p/B9pXxELlsQr/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_004.jpg" className="fIItem" alt="Instagram_004" />
+              </a>
+            </Slide>
+            <Slide right>
+              <a href="https://www.instagram.com/p/CAMOnUYJQbL/" className="fIItemContainer" target="_blank" rel="noopener noreferrer">
+                <img src="/images/pages/insta_005.jpg" className="fIItem" alt="Instagram_005" />
               </a>
             </Slide>
         </Carousel>
