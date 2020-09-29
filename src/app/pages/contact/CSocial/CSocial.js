c3eb6783e80423966e9f7303d9dd134810816708
Blaine Anderson <BlaineAndersonDev@Gmail.com>
2020-09-28 17:36:20 -0700

    Updated social icon styles to client requirements. Smallers, with tooltips

--- src/app/pages/contact/CSocial/CSocial.js
+++ src/app/pages/contact/CSocial/CSocial.js
@@ -6,16 +6,19 @@ class CSocial extends Component {
     return (
       <div id="cSocial">
         
-        <a href="https://www.facebook.com/SkywardTherapies/" target="_blank" rel="noopener noreferrer" className="cIconContainer cFB">
+        <a href="https://www.facebook.com/SkywardTherapies/" target="_blank" rel="noopener noreferrer" className="cIconContainer cFB tooltip">
           <img src="/images/pages/social_facebook.png" alt="Facebook-Icon" className="cIcon" />
+          <span class="tooltiptext">Check us out on Facebook</span>
         </a>
 
-        <a href="https://www.massagebook.com/biz/SkywardTherapies#services" target="_blank" rel="noopener noreferrer" className="cIconContainer cMassageBook">
+        <a href="https://www.massagebook.com/biz/SkywardTherapies#services" target="_blank" rel="noopener noreferrer" className="cIconContainer cMassageBook tooltip">
           <img src="/images/pages/social_massagebook.png" alt="GMaps-Icon" className="cIcon" />
+          <span class="tooltiptext">Book an appointment on Massagebook</span>
         </a>
 
-        <a href="https://www.instagram.com/skyward.therapies.nutrition/" target="_blank" rel="noopener noreferrer" className="cIconContainer cInsta">
+        <a href="https://www.instagram.com/skyward.therapies.nutrition/" target="_blank" rel="noopener noreferrer" className="cIconContainer cInsta tooltip">
           <img src="/images/pages/social_instagram.png" alt="Insta-Icon" className="cIcon" />
+          <span class="tooltiptext">Follow us on Instagram</span>
         </a>
 
       </div>
