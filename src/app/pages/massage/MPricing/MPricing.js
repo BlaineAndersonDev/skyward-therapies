69463f29372bc2fd0b98e00a7045b6b219b3edb6
Blaine Anderson <BlaineAndersonDev@Gmail.com>
2021-07-17 11:07:36 -0700

    Updated CSS

--- src/app/pages/massage/MPricing/MPricing.js
+++ src/app/pages/massage/MPricing/MPricing.js
@@ -10,8 +10,9 @@ class MPricing extends Component {
           <li className="mPricingListItem">60 Minute Massage: $85</li>
           <li className="mPricingListItem">90 Minute Massage: $130</li>
           <li className="mPricingListItem">120 Minute Massage: $170</li>
-          <li className="mPricingListItem">No Gratuity Necessary.</li>
+          <li className="mPricingListItem">.</li>
         </ul>
+        <div className="mGratuity">~ No Gratuity Necessary ~</div>
       </div>
     );
   }
