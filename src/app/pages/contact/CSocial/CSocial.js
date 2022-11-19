90adbf6b8d1fadb2b52eeda3ff163ee5a2567ee5
Blaine Anderson <BlaineAndersonDev@gmail.com>
2022-11-19 14:31:43 -0700

    Minor Update to instagram
    Updated Instagram Link.

--- src/app/pages/contact/CSocial/CSocial.js
+++ src/app/pages/contact/CSocial/CSocial.js
@@ -5,7 +5,7 @@ class CSocial extends Component {
   render() {
     return (
       <div id="cSocial">
-        
+
         <a href="https://www.facebook.com/SkywardTherapies/" target="_blank" rel="noopener noreferrer" className="cIconContainer cFB tooltip">
           <img src="/images/pages/social_facebook.png" alt="Facebook-Icon" className="cIcon" />
           <span className="tooltiptext">Check us out on Facebook</span>
@@ -16,7 +16,7 @@ class CSocial extends Component {
           <span className="tooltiptext">Book an appointment on Massagebook</span>
         </a>
 
-        <a href="https://www.instagram.com/skyward.therapies.nutrition/" target="_blank" rel="noopener noreferrer" className="cIconContainer cInsta tooltip">
+        <a href="https://www.instagram.com/skyward.therapies/" target="_blank" rel="noopener noreferrer" className="cIconContainer cInsta tooltip">
           <img src="/images/pages/social_instagram.png" alt="Insta-Icon" className="cIcon" />
           <span className="tooltiptext">Follow us on Instagram</span>
         </a>
