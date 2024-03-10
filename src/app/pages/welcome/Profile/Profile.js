0b20dc34fbbbdfdaf2a79af13436d5ab033f3911
Blaine Anderson <BlaineAndersonDev@gmail.com>
2024-03-10 13:42:38 -0700

    Updated Profile

--- src/app/pages/welcome/Profile/Profile.js
+++ src/app/pages/welcome/Profile/Profile.js
@@ -6,16 +6,18 @@ class Profile extends Component {
     return (
       <div id="profile">
         <div className="profileTextContainer">
-          <p className="profileText">My name is Kelli Anderson LMT, MMP, NDTR. I am so happy you are here! Simply by being on this page you are:</p>
-          <ul className="profileListContainer">
+          <h1 className="profileTitle">I am so happy you are here!</h1>
+          <p className="profileText">I'm Kelli Anderson, your dedicated Licensed Massage Therapist (LMT) and Nutrition Expert.</p>
+          <p className="profileText">With professional certifications as a Medical Massage Practitioner (MMP) and a Nutrition and Dietetic Technician (NDTR), I specialize in holistic health solutions.</p>
+          <p className="profileText">By visiting my site, you're taking a significant step towards embracing self-care routines, investing in your health and well-being, and prioritizing your mental and physical health.</p>
+          {/* <ul className="profileListContainer">
             <li className="profileListText">Ready to embrace your own self care,</li>
             <li className="profileListText">Ready to invest in improving your health,</li>
             <li className="profileListText">And most importantly,</li>
             <li className="profileListText">Ready to make yourself a PRIORITY!</li>
-          </ul>
-          <p className="profileText">I am a Licensed Medical Massage Therapist and a Nutrition and Dietetic Technician.</p>
-          <p className="profileText">My main goal is to help you in your journey of wellness!</p>
-          <p className="profileText">Whether you are seeking a few hours of quiet time a week on a massage table, far away from your worries, or eating advice to improve your daily health, I am here and ready to help!</p>
+          </ul> */}
+          <p className="profileText">I am committed to assisting you on your wellness journey.</p>
+          <p className="profileText">Whether you're seeking therapeutic massage services to alleviate stress, recovering from injury, or looking for personalized nutrition advice to enhance your lifestyle, I am here to support your health goals.</p>
         </div>
       </div>
     );
