3416121749d2f03f59fbb719d9514dfcdb1f55ae
Blaineandersondev <BlaineAndersonDev@Gmail.com>
2025-10-24 15:09:24 -0700

    Another new image

--- src/app/pages/landing/Logo/Logo.js
+++ src/app/pages/landing/Logo/Logo.js
@@ -6,7 +6,8 @@ class Logo extends Component {
     return (
       <img
         // src="/images/pages/2025_Logo_Cloud.png"
-        src="/images/pages/2025_Logo_Rec.png"
+        // src="/images/pages/2025_Logo_Rec.png"
+        src="/images/pages/2025_Logo_Mini_Recs.png"
         alt="skywardLogo"
         id="skywardLogo"
       />
